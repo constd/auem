b6677872ba7125d384f172bc480698cc206faeec
Christopher Jacoby <christopher.jacoby@gmail.com>
2020-03-31 10:44:36 -0700

    Adds poetry and basic test.

---
+++ README.md
@@ -0,0 +1,1 @@
+# AUEM - Audio Embedding
---
+++ tests/test_auem_install.py
@@ -0,0 +1,8 @@
+"""These tests are trivial and should be removed when more detailed tests are added."""
+
+def test_can_import_base():
+    import auem
+
+
+def test_can_import_datasets():
+    import auem.data.datasets
