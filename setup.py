7384d2f350c1720e65372c3ef135a3ad131b7535
Konstantinos Dimitriou <constd@outlook.com>
2020-04-06 09:36:15 +0000

    added some extra config for hydra

--- setup.py
+++ setup.py
@@ -7,11 +7,12 @@ setuptools.setup(
     author_email="const@embeddingspace.com",
     url="https://github.com/constd/auem",
     packages=setuptools.find_packages(),
+    entry_points={"console_scripts": ["auem = auem.train:entry"]},
     classifiers=[
         "Programming Language :: Python :: 3",
         "Topic :: Multimedia :: Sound/Audio :: Analysis",
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires=">=3.6",
 )
