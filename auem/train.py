3fe7d84eb2bc2d0dece24ce10ebedba6a4341518
Christopher Jacoby <christopher.jacoby@gmail.com>
2020-04-26 11:16:17 -0700

    migrates common functionality to be simply imported from 'train.py', and now passes the batch input shape to model creation.

--- auem/train.py
+++ auem/train.py
@@ -1,14 +1,17 @@
+"""Train a pytorch model using a pytorch Dataset with a known length."""
 import logging
 import os
 from math import ceil
-from typing import Tuple
+from typing import NamedTuple, Tuple
 
 import hydra
 import torch
+import torch.nn as nn
 from omegaconf import DictConfig
 from torch.cuda import is_available as is_cuda_available
 from torch.utils import tensorboard as tb
 from torch.utils.data import DataLoader, Dataset
+
 from tqdm import tqdm
 
 # import auem.evaluation.confusion as confusion
@@ -17,13 +20,14 @@ logger = logging.getLogger(__name__)
 
 
 def datasets(cfg: DictConfig) -> Tuple[Dataset]:
+    """Create dataset objects using the config."""
     transforms = hydra.utils.instantiate(cfg.transform)
     ds_train = hydra.utils.get_class(cfg.dataset["class"])(
         audioset_annotations=cfg.dataset["folds"]["train"],
         transforms=transforms,
         **cfg.dataset.params,
     )
-    ds_valid = hydra.utils.get_class(cfg.dataset["class"])(
+    ds_valid = hydra.utils.gget_class(cfg.dataset["class"])(
         audioset_annotations=cfg.dataset["folds"]["val"],
         transforms=transforms,
         **cfg.dataset.params,
@@ -34,6 +38,7 @@ def datasets(cfg: DictConfig) -> Tuple[Dataset]:
 def dataloaders(
     cfg: DictConfig, ds_train: Dataset, ds_valid: Dataset
 ) -> Tuple[DataLoader]:
+    """Create dataloader objects using the config and datasets."""
     dl_train = hydra.utils.get_class(cfg.dataloader["class"])(
         ds_train, **cfg.dataloader.params
     )
@@ -43,14 +48,36 @@ def dataloaders(
     return (dl_train, dl_valid)
 
 
-def train(cfg: DictConfig) -> None:
-    device = cfg.cuda.device if cfg.cuda.enable and is_cuda_available() else "cpu"
+class TrainingSetupTuple(NamedTuple):
+    """A TrainingSetup namedtuple.
 
-    ds_train, ds_valid = datasets(cfg)
-    dl_train, dl_valid = dataloaders(cfg, ds_train, ds_valid)
+    Used to enforce type and make the result of `training_setup` accessible
+    by property/name.
+    """
 
-    model = hydra.utils.instantiate(cfg.model).to(device)
+    optimizer: torch.optim.Optimizer
+    scheduler: torch.optim.lr_scheduler._LRScheduler
+    criterion: nn.modules.loss._Loss
 
+
+def training_setup(cfg: DictConfig, model: nn.Module) -> TrainingSetupTuple:
+    """Create non-data, non-model training components.
+
+    (eg. optimizer, scheduler, criteron, ...)
+
+    Parameters
+    ----------
+    cfg : DictConfig
+        The complete config.
+
+    model : nn.Module
+        Instantiated model.
+
+    Returns
+    -------
+    optimizer, schedule, criterion
+        Tuple of the above, instantiated.
+    """
     optimizer = hydra.utils.get_class(cfg.optim["class"])(
         model.parameters(), **cfg.optim.params
     )
@@ -58,12 +85,25 @@ def train(cfg: DictConfig) -> None:
         optimizer, **cfg.scheduler.params
     )
     criterion = hydra.utils.instantiate(cfg.criterion)
+    return TrainingSetupTuple(optimizer, scheduler, criterion)
+
+
+def train(cfg: DictConfig) -> None:
+    """Train a model given a configuration from the config."""
+    device = cfg.cuda.device if cfg.cuda.enable and is_cuda_available() else "cpu"
+
+    ds_train, ds_valid = datasets(cfg)
+    dl_train, dl_valid = dataloaders(cfg, ds_train, ds_valid)
+    example_batch = iter(dl_train).next()
+
+    model = hydra.utils.instantiate(cfg.model, example_batch["X"].shape).to(device)
+    optimizer, scheduler, criterion = training_setup(cfg, model)
 
     num_batches_train = ceil(len(ds_train) / cfg.dataloader.params.batch_size)
     num_batches_valid = ceil(len(ds_valid) / cfg.dataloader.params.batch_size)
 
     writer = tb.SummaryWriter()
-    writer.add_graph(model, iter(dl_train).next()["X"].to(device))
+    writer.add_graph(model, example_batch["X"].to(device))
     for epoch in tqdm(range(cfg.epochs), position=0, desc="Epoch"):
         losses = 0
         model.train()
@@ -119,6 +159,7 @@ def train(cfg: DictConfig) -> None:
 
 @hydra.main(config_path="config/config.yaml")
 def main(cfg: DictConfig) -> None:
+    """Execute Training using hydra configs."""
     import git
 
     repo = git.Repo(search_parent_directories=True)
