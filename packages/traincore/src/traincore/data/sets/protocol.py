6b7616645144c663b941e367f945431d0bf90608
Constantinos Dimitriou <constd@users.noreply.github.com>
2025-09-19 11:15:55 -0300

    test: add `runtime_checkable` to protocols for type checking

--- packages/traincore/src/traincore/data/sets/protocol.py
+++ packages/traincore/src/traincore/data/sets/protocol.py
@@ -1,17 +1,20 @@
-from typing import Iterator, Protocol
+from typing import Iterator, Protocol, runtime_checkable
 
 from torch import Tensor
 
 
+@runtime_checkable
 class DatasetProtocol(Protocol):
     def setup(self, stage: str | None) -> None: ...
     def prepare_data(self) -> None: ...
 
 
-class MapDatasetProtocol(DatasetProtocol):
+@runtime_checkable
+class MapDatasetProtocol(DatasetProtocol, Protocol):
     def __len__(self) -> int: ...
-    def __getitem__(self, index: int) -> dict[str, str, Tensor]: ...
+    def __getitem__(self, index: int) -> dict[str, str | Tensor]: ...
 
 
-class IterableDatasetProtocol(DatasetProtocol):
-    def __iter__(self) -> Iterator[dict[str, str, Tensor]]: ...
+@runtime_checkable
+class IterableDatasetProtocol(DatasetProtocol, Protocol):
+    def __iter__(self) -> Iterator[dict[str, str | Tensor]]: ...
--- packages/traincore/src/traincore/models/encoders/protocol.py
+++ packages/traincore/src/traincore/models/encoders/protocol.py
@@ -1,7 +1,8 @@
-from typing import Protocol
+from typing import Protocol, runtime_checkable
 
 from torch import Tensor
 
 
+@runtime_checkable
 class EncoderProtocol(Protocol):
     def forward(self, *args, **kwargs) -> Tensor: ...
