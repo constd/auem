08e4895f819465e5ef70d33b296f284b3e6ed7cc
Constantinos Dimitriou <constd@users.noreply.github.com>
2025-09-23 03:44:07 -0300

    refactor: add SimpleCNNBase to core models

--- packages/traincore/src/traincore/models/__init__.py
+++ packages/traincore/src/traincore/models/__init__.py
@@ -1,8 +1,7 @@
 """Pytorch models and model components for audio."""
 
 from traincore.models.resnet import SpectrogramResNet
-
-# from .simplecnn import SimpleCNNBase
+from traincore.models.simplecnn import SimpleCNNBase
 from traincore.models.simplenn import SimpleNN
 
-__all__ = ["SpectrogramResNet", "SimpleNN"]  # , "SimpleCNNBase", "SimpleNN"]
+__all__ = ["SpectrogramResNet", "SimpleNN", "SimpleCNNBase"]
--- packages/traincore/tests/models/test_simplecnn.py
+++ packages/traincore/tests/models/test_simplecnn.py
@@ -1,52 +1,52 @@
-# """Tests simplecnn model."""
+"""Tests simplecnn model."""
 
-# import pytest
-# import torch
+import pytest
+import torch
 
-# from auem.models.simplecnn import SimpleCNNBase
+from traincore.models.simplecnn import SimpleCNNBase
 
 
-# @pytest.mark.parametrize("feature_shape", [(None, 1, 128, 256), (None, 1, 128, 64)])
-# @pytest.mark.parametrize("n_classes", [2, 10])
-# @pytest.mark.parametrize(
-#     "conv_def",
-#     [
-#         [  # this is the default
-#             (8, 7, (2,)),
-#             (16, 5, (1, 2)),
-#             (32, 3, (1, 2)),
-#             (32, 4, (2, 3)),
-#         ],
-#         [(8, 7, (2,))],
-#         [
-#             (
-#                 8,
-#                 7,
-#                 [
-#                     2,
-#                 ],
-#             )
-#         ],  # noqa: E231
-#         [(8, 7, (2,)), (16, 3, (2, 3))],
-#     ],
-# )
-# def test_create_model(feature_shape, n_classes, conv_def):
-#     """Try creating the cnn model with various parameters."""
-#     model = SimpleCNNBase(feature_shape, num_classes=n_classes, conv_layer_def=conv_def)
-#     criterion = torch.nn.BCEWithLogitsLoss()
+@pytest.mark.parametrize("feature_shape", [(None, 1, 128, 256), (None, 1, 128, 64)])
+@pytest.mark.parametrize("n_classes", [2, 10])
+@pytest.mark.parametrize(
+    "conv_def",
+    [
+        [  # this is the default
+            (8, 7, (2,)),
+            (16, 5, (1, 2)),
+            (32, 3, (1, 2)),
+            (32, 4, (2, 3)),
+        ],
+        [(8, 7, (2,))],
+        [
+            (
+                8,
+                7,
+                (2,),
+            )
+        ],
+        [(8, 7, (2,)), (16, 3, (2, 3))],
+    ],
+)
+def test_create_model(feature_shape, n_classes, conv_def):
+    """Try creating the cnn model with various parameters."""
+    model = SimpleCNNBase(
+        input_shape=feature_shape, num_classes=n_classes, conv_layer_def=conv_def
+    )
+    criterion = torch.nn.BCEWithLogitsLoss()
 
-#     batch_shape = (8,) + feature_shape[1:]
-#     targets = torch.randint(n_classes, (8,))
-#     y = torch.zeros(8, n_classes)
-#     y[range(8), targets] = 1
-#     batch = torch.rand(*batch_shape)
+    batch_shape = (8,) + feature_shape[1:]
+    targets = torch.randint(n_classes, (8,))
+    y = torch.zeros(8, n_classes)
+    y[range(8), targets] = 1
+    batch = torch.rand(*batch_shape)
 
-#     model.train()
-#     y_hat = model(batch)
-#     assert len(y_hat) == len(batch)
-#     assert y_hat.shape[1] == n_classes
+    model.train()
+    y_hat = model(batch)
+    assert len(y_hat) == len(batch)
+    assert y_hat.shape[1] == n_classes
 
-#     # Make sure we can update the weights successfully.
-#     # loss = torch.nn.NLLLoss(model.parameters())
-#     loss = criterion(y_hat, y)
-#     loss.backward()
+    # Make sure we can update the weights successfully.
+    # loss = torch.nn.NLLLoss(model.parameters())
+    loss = criterion(y_hat, y)
+    loss.backward()
