f5f759b159a3a88d15d2ba6bd77bc174a9eb8922
Christopher Jacoby <christopher.jacoby@gmail.com>
2025-09-23 20:59:41 -0700

    feat: adds basic config up to recipe

--- tests/test_train.py
+++ tests/test_train.py
@@ -3,13 +3,33 @@ import auem.train
 
 
 def test_binary_classification_with_random_data():
+    dataset_config = {
+        "_target_": "traincore.data.sets.random.RandomAudioWithClassifierDataset",
+        "n_samples": 22050,
+        "n_classes": 2,
+    }
     config = DictConfig(
         {
             "logger": None,
-            "data": None,
-            "model": None,
+            "data": {
+                "_target_": "traincore.data.modules.generic.BasicDataModule",
+                "datasets": {
+                    "train": {"n_examples": 20, **dataset_config},
+                    "validation": {"n_examples": 5, **dataset_config},
+                    "test": None,
+                },
+            },
+            "model": {
+                "_target_": "traincore.models.simplecnn.SimpleCNNBase",
+                "encoder": {
+                    "_target_": "traincore.models.encoders.melspec.MelEncoder",
+                    "n_mels": 80,
+                },
+                # "input_shape": (1, 1, 80, 11),
+                "num_classes": 2,
+            },
             "recipe": None,
-            "trainer": "lightning.pytorch.trainer.trainer.Trainer",
+            "trainer": {"_target_": "lightning.pytorch.trainer.trainer.Trainer"},
         }
     )
 
