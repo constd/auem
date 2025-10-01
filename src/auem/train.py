8e7a3c58f94e01cc3f4bdd178aa53d6d95230099
Constantinos Dimitriou <constd@users.noreply.github.com>
2025-10-01 11:41:59 -0700

    feat: make train.py executable for direct invocation, remove hydra decorator

--- src/auem/train.py
+++ src/auem/train.py
@@ -1,13 +1,17 @@
 # given a config, set up all the necessary parts, and run training
+from hydra import compose, initialize
 from hydra_zen import instantiate
 from lightning.pytorch import LightningDataModule, LightningModule, Trainer
 from lightning.pytorch.loggers import Logger
 from omegaconf import DictConfig
 from torch.nn import Module
 
+from auem.configs.mainconfig import train_store  # noqa
+
 
 def train(config: DictConfig) -> None:
-    loggers: Logger = instantiate(config.logger)
+    if config.get("logger", None) is not None:
+        loggers: Logger = instantiate(config.logger)
 
     data: LightningDataModule = instantiate(config.data)
 
@@ -18,3 +22,15 @@ def train(config: DictConfig) -> None:
     trainer: Trainer = instantiate(config.trainer, logger=loggers)
 
     trainer.fit(recipe, data)
+
+
+if __name__ == "__main__":
+    initialize(
+        config_path="auem/configs",
+        job_name="train",
+        version_base="1.3",
+    )
+    config = compose(
+        config_name="train_config", overrides=["+experiment=tests/classification"]
+    )
+    train(config)
