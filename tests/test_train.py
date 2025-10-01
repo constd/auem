6f854b4e2c4f921c26f8f1d45f6e9346bc328709
Constantinos Dimitriou <constd@users.noreply.github.com>
2025-10-01 11:41:04 -0700

    test: add classification experiment config and test

--- tests/test_train.py
+++ tests/test_train.py
@@ -1,36 +1,16 @@
-# from omegaconf import DictConfig
-# import auem.train
+from hydra import compose, initialize
+
+import auem.train
 
 
-# def test_binary_classification_with_random_data():
-#     dataset_config = {
-#         "_target_": "traincore.data.sets.random.RandomAudioWithClassifierDataset",
-#         "n_samples": 22050,
-#         "n_classes": 2,
-#     }
-#     config = DictConfig(
-#         {
-#             "logger": None,
-#             "data": {
-#                 "_target_": "traincore.data.modules.generic.BasicDataModule",
-#                 "datasets": {
-#                     "train": {"n_examples": 20, **dataset_config},
-#                     "validation": {"n_examples": 5, **dataset_config},
-#                     "test": None,
-#                 },
-#             },
-#             "model": {
-#                 "_target_": "traincore.models.simplecnn.SimpleCNNBase",
-#                 "encoder": {
-#                     "_target_": "traincore.models.encoders.melspec.MelEncoder",
-#                     "n_mels": 80,
-#                 },
-#                 # "input_shape": (1, 1, 80, 11),
-#                 "num_classes": 2,
-#             },
-#             "recipe": None,
-#             "trainer": {"_target_": "lightning.pytorch.trainer.trainer.Trainer"},
-#         }
-#     )
+def test_binary_classification_with_random_data():
+    initialize(
+        config_path="../src/auem/configs",
+        job_name="test",
+        version_base="1.3",
+    )
+    config = compose(
+        config_name="train_config", overrides=["+experiment=tests/classification"]
+    )
 
-#     auem.train.train(config)
+    auem.train.train(config)
