b68da7f8ac4f0e9e116ed062e03a9cf1e3c26a25
Constantinos Dimitriou <constd@users.noreply.github.com>
2025-11-06 16:41:39 -0800

    feat: snake activation

--- packages/traincore/src/traincore/models/activations/__init__.py
+++ packages/traincore/src/traincore/models/activations/__init__.py
@@ -1,3 +1,4 @@
 from traincore.models.activations.afa import Activation1d, Activation2d
+from traincore.models.activations.snake import Snake, SnakeBeta
 
-__all__ = ["Activation1d", "Activation2d"]
+__all__ = ["Activation1d", "Activation2d", "Snake", "SnakeBeta"]
